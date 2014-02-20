287115aaf810befc9cef3fea97ed57b93a615ff4
Jean-Sebastien Dery <jeansebastien.dery@gmail.com>
2014-02-19 21:26:12 -0500

    Set up the basic stuff required for the assignment

---
+++ src/MagicTriangle.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+
+/**
+ * @author Jean-Sebastien Dery
+ * @author Renaud Dagenais
+ *
+ */
+public class MagicTriangle {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+	}
+}
