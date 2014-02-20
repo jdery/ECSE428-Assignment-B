bdd4770a7f2c4f1699739b5d0233d3a321426aac
Jean-Sebastien Dery <jeansebastien.dery@gmail.com>
2014-02-19 22:07:31 -0500

    Changed the JUnit class

--- test/TestMagicTriangle.java
+++ test/TestMagicTriangle.java
@@ -6,27 +6,65 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+/**
+ * 
+ */
 
+/**
+ * @author dery
+ *
+ */
 public class TestMagicTriangle {
 
+	/**
+	 * @throws java.lang.Exception
+	 */
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
 	}
 
+	/**
+	 * @throws java.lang.Exception
+	 */
 	@AfterClass
 	public static void tearDownAfterClass() throws Exception {
 	}
 
+	/**
+	 * @throws java.lang.Exception
+	 */
 	@Before
 	public void setUp() throws Exception {
 	}
 
+	/**
+	 * @throws java.lang.Exception
+	 */
 	@After
 	public void tearDown() throws Exception {
 	}
 
+	/**
+	 * Test method for {@link MagicTriangle#main(java.lang.String[])}.
+	 */
 	@Test
-	public void test() {
+	public void testMain() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link MagicTriangle#isValidEntries(java.lang.String[])}.
+	 */
+	@Test
+	public void testIsValidEntries() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link MagicTriangle#findWhichTypeOfTriangle()}.
+	 */
+	@Test
+	public void testFindWhichTypeOfTriangle() {
 		fail("Not yet implemented");
 	}
 
